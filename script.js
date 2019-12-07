c200be05964338a39c9c930514ce99283c12a26a
charlesmartinreed <charlesmartinreed@icloud.com>
2019-12-07 16:45:33 -0600

    Set up msg broadcasting. Display recvd msg in DOM.

--- script.js
+++ script.js
@@ -2,6 +2,30 @@
 // get socket variable from the server host location
 const socket = io("http://localhost:4000");
 
+const messageContainer = document.getElementById("message-container");
+const messageForm = document.getElementById("send-container");
+const messageInput = document.getElementById("message-input");
+
 socket.on("chat-message", data => {
-  console.log(data);
+  appendMessage(data);
 });
+
+messageForm.addEventListener("submit", e => {
+  // we need to prevent the default submit action so that we don't lose our chat messages when the page refreshes.
+  e.preventDefault();
+
+  //   grab the message
+  const message = messageInput.value;
+
+  //   send from client to server
+  socket.emit("send-chat-message", message);
+
+  //   clear the input field
+  messageInput.value = "";
+});
+
+const appendMessage = msg => {
+  const messageElement = document.createElement("div");
+  messageElement.textContent = msg;
+  messageContainer.appendChild(messageElement);
+};
--- server.js
+++ server.js
@@ -3,6 +3,11 @@ const io = require("socket.io")(4000);
 
 io.on("connection", socket => {
   // when a user connects, we emit the 'chat message' and pass along the data payload we want to send down to the client
-  console.log("new user connected");
   socket.emit("chat-message", "Hello World");
+
+  //   receive chat messages
+  socket.on("send-chat-message", message => {
+    //   broadcast sends to all other sockets except your own - we'll send the message to everyone else
+    socket.broadcast.emit("chat-message", message);
+  });
 });

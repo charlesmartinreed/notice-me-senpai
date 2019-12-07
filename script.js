5c4d364f54b160ea244f4ef3033a7a31e710c74f
charlesmartinreed <charlesmartinreed@icloud.com>
2019-12-07 17:24:03 -0600

    Add random username generator when name not chosen.

--- script.js
+++ script.js
@@ -7,7 +7,48 @@ const messageForm = document.getElementById("send-container");
 const messageInput = document.getElementById("message-input");
 
 // BASIC USER SETUP - flesh this out later
-const username = prompt("Please choose a username");
+const username = prompt("Please choose a username") || generateUsername();
+console.log(username, typeof username);
+
+function generateUsername() {
+  let adjectives = [
+    "Timid",
+    "Hasty",
+    "Adamant",
+    "Bold",
+    "Hasty",
+    "Quiet",
+    "Quirky",
+    "Rash",
+    "Lonely",
+    "Jolly",
+    "Bashful",
+    "Sassy"
+  ];
+  let nouns = [
+    "Ninetales",
+    "Weavile",
+    "Popplio",
+    "Bidoof",
+    "Dunsparce",
+    "Gliscor",
+    "Gogoat",
+    "Xerneas",
+    "Donphan",
+    "Mudbray",
+    "Mimikyu",
+    "Volcarona",
+    "Lugia",
+    "Umbreon"
+  ];
+
+  let randomAdjective =
+    adjectives[Math.floor(Math.random() * adjectives.length)];
+  let randomNoun = nouns[Math.floor(Math.random() * nouns.length)];
+
+  return `${randomAdjective} ${randomNoun}`;
+}
+
 appendMessage("You joined the room");
 socket.emit("new-user", username);
 
@@ -31,6 +72,8 @@ messageForm.addEventListener("submit", e => {
 
   //   clear the input field
   messageInput.value = "";
+
+  appendMessage(`You: ${message}`);
 });
 
 function appendMessage(msg) {
